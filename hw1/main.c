358bcb3d0f3a9059f1b00905a83622a1afc38e21
Rubin Jha <wwg6yr@virginia.edu>
2024-02-02 16:32:50 -0500

    the

--- hw1/main.c
+++ hw1/main.c
@@ -1,28 +1,49 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-int main(int argc, char *argv[]) {
-        char *sep;
-        if(argc < 2){
-            sep = " \t";
-        }
-        else{
-        sep = argv[1];
-        }
-        int *num_words;
-        char inp[4000];
-        char **splitup;
-        while(strcmp(inp, ".") != 0){ 
-        //read input, call string split to split it up, print out input,  until .
-        fgets(inp, sizeof(inp), stdin);
-        splitup = string_split(inp, sep, &num_words);
-        //call function
-        printf("%s", splitup);
-        }
-        for(int i = 0; i < num_words; i++){
-        free(splitup[i]);
-        }
-        free(splitup);
-        
+#include <stdio.h>
+#include "split.h"
+int main(int argc, char *argv[])
+{
+    char *sep;
+    if (argc < 2)
+    {
+        sep = " \t";
     }
- 
+    else{
+    size_t totalSize = 0;
+    for (int i = 1; i < argc; i++) {
+        totalSize += strlen(argv[i]);
+    }
+
+    sep = (char *)malloc(totalSize + 1);
+
+    sep[0] = '\0'; // Ensure the string is initially empty
+    for (int i = 1; i < argc; i++) {
+        strcat(sep, argv[i]);
+    }
+
+    }  
+
+    int num_words;
+    char *inp = (char *)malloc(4000);
+    char **splitup;
+    while (1)
+    {
+        // read input, call string split to split it up, print out input,  until .
+        fgets(inp, sizeof(inp), stdin);
+        if(strcmp(inp, ".") != 0){
+            break;
+        }
+        splitup = string_split(inp, sep, &num_words);
+        // call function
+        for(int i = 0; i < sizeof(splitup); i++){
+        printf("%s \n", splitup[i]);
+        }
+    }
+    for (int i = 0; i < num_words; i++)
+    {
+        free(splitup[i]);
+    }
+    free(splitup);
+}
--- hw1/split.c
+++ hw1/split.c
@@ -1,42 +1,47 @@
 #include "split.h"
 #include <stdlib.h>
 #include <string.h>
-char **string_split(const char *input, const char *sep, int *num_words){
+#include <stdio.h>
+char **string_split(const char *input, const char *sep, int *num_words)
+{
 
-// nested for loop that goes checks input until it reaches null terminator
-char **array = (char **)malloc(sizeof(char *));
-//iterator for input
-int i = 0;
+   // nested for loop that goes checks input until it reaches null terminator
+   char **array = (char **)malloc(sizeof(char *));
+   // iterator for input
+   int i = 0;
 
-//size of word and seperator
-int wordLen = 0;
-// index of array
-   if(strspn(input, sep) == 0){
+   // size of word and seperator
+   int wordLen = 0;
+   // index of array
+   *num_words = 0;
+   if (strspn(input, sep) == 0)
+   {
+      printf("%d \n", *num_words);
       // size of the a
-      array[*num_words] = (char *)malloc(sizeof(char) * (*num_words+1));
+      array[*num_words] = (char *)malloc(sizeof(char) * ((*num_words) + 1));
       strcpy(array[0], "");
-      num_words++;
+      (*num_words)++;
       i += strcspn(input, sep);
-      }
+   }
 
-   while(input[i] != '\0'){
+   while (input[i] != '\0')
+   {
       wordLen = strspn(input + i, sep);
       array = (char **)realloc(array, sizeof(char *));
-      array[wordLen] = (char *)malloc(sizeof(char)*(wordLen+1));
-      *num_words++;
-      strncpy(array[*num_words], input+i, wordLen);
+      array[wordLen] = (char *)malloc(sizeof(char) * (wordLen + 1));
+      (*num_words)++;
+      strncpy(array[(*num_words)], input + i, wordLen);
       i += wordLen;
-      
+
       wordLen = strcspn(input, sep);
       i += wordLen;
 
-      if(input[i+wordLen] == '\0'){
-      array = (char **)realloc(array, sizeof(char *));
-      array[wordLen] = (char *)malloc(sizeof(char)*(wordLen+1));
-      strcpy(array[*num_words], "");
+      if (input[i + wordLen] == '\0')
+      {
+         array = (char **)realloc(array, sizeof(char *));
+         array[wordLen] = (char *)malloc(sizeof(char) * (wordLen + 1));
+         strcpy(array[(*num_words)], "");
       }
    }
-      return array;
- }
-
-
+   return array;
+}
