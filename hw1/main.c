54695be160f30156ddd6dece1679007d07e999ae
Rubin Jha <wwg6yr@virginia.edu>
2024-02-02 20:42:56 -0500

    We

--- hw1/main.c
+++ hw1/main.c
@@ -6,6 +6,7 @@
 int main(int argc, char *argv[])
 {
     char *sep;
+    int sepmall = 0;
     if (argc < 2)
     {
         sep = " \t";
@@ -19,7 +20,7 @@ int main(int argc, char *argv[])
         }
 
         sep = (char *)malloc(totalSize + 1);
-
+        sepmall = 1;
         sep[0] = '\0'; // Ensure the string is initially empty
         for (int i = 1; i < argc; i++)
         {
@@ -30,7 +31,7 @@ int main(int argc, char *argv[])
     int num_words;
     char *inp = (char *)malloc(4000);
     char **splitup;
-    bool splitup_malloced = false;
+    int splitup_malloced = 0;
     while (1)
     {
         // read input, call string split to split it up, print out input,  until .
@@ -44,9 +45,8 @@ int main(int argc, char *argv[])
         }
         inp[strlen(inp) -1] = '\0';
         splitup = string_split(inp, sep, &num_words);
-        splitup_malloced = true;
+        splitup_malloced = 1;
         // call function
-        printf("Num words: %d", num_words);
         for (int i = 0; i < num_words; i++)
         {
             printf("[%s]\n", splitup[i]);
@@ -58,6 +58,9 @@ int main(int argc, char *argv[])
     }
     if(splitup_malloced){
         free(splitup);
+    }
+    if(sepmall){
+        free(sep);
     }
     free(inp);
 }
