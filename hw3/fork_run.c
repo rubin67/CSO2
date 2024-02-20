822bc322144496ae0c1aa4225bbfbc22c173d839
Rubin Jha <wwg6yr@virginia.edu>
2024-02-20 14:09:59 -0500

    T

--- hw3/fork_run.c
+++ hw3/fork_run.c
@@ -13,12 +13,12 @@ char *getoutput(const char *command)
     pid_t child_pid = getpid();
     int pipe_fd[2];
     if (pipe(pipe_fd) < 0)
-        handle_error(); /* e. g. out of file descriptors */
+        perror("pipe error"); /* e. g. out of file descriptors */
     int read_fd = pipe_fd[0];
     int write_fd = pipe_fd[1];
     pid_t pid = fork();
 
-    if (child_pid == 0)
+    if (pid == 0)
     {
         /* in child process, write to pipe */
         dup2(pipe_fd[1], STDOUT_FILENO);
@@ -28,36 +28,53 @@ char *getoutput(const char *command)
         /* function not shown */
         exit(EXIT_SUCCESS);
     }
-    else if (child_pid > 0){
+    else if (pid > 0){
         /* in parent process, read from pipe */
         close(write_fd);
         //need to implement this read thing
-        char* buffer = (char*)malloc(4096);
-        
-        read(read_fd, buffer, sizeof(buffer));
+        // char* buffer = (char*)malloc(4096);
+        // size_t bytes_read;
+        char *buffer = NULL; 
+        size_t buffer_length = 0;
+        FILE *theFile = fdopen(read_fd, "r");
 
-        close(read_fd);
+        getdelim(&buffer, &buffer_length, '\0', theFile);
+        fclose(theFile);
         waitpid(child_pid, NULL, 0);
-    } else {
-        return -1;
+        return buffer;
     }
+    else
+    return 0;
 }
 char *parallelgetoutput(int count, const char **argv_base)
 {
     //multiple child processes executing at the same time
     //use for loop to handle child processes
+    int pipe_fd[2];
+    if (pipe(pipe_fd) < 0)
+        perror("pipe error"); /* e. g. out of file descriptors */
+    int read_fd = pipe_fd[0];
+    int write_fd = pipe_fd[1];
+
     for(int i = 0; i <= count; i++){
+    
     pid_t pid = fork();
-    int child_pid = getpid();
-    if (child_pid == 0)
+    if (pid == 0)
     {
-        char** temp = (char**)malloc(sizeof(char*)*sizeof(argv_base+2));
+        /* in child process, write to pipe */
+        dup2(pipe_fd[1], STDOUT_FILENO);
+        close(read_fd);
+        close(write_fd);
+        //handle pointer to pointer as opposed to just pointer
+        execv(argv_base[0], argv_base);
+        /* function not shown */
+        exit(EXIT_SUCCESS);
+        //char** temp = (char**)malloc(sizeof(char*)*sizeof(argv_base+2));
         printf(0);
-        execv(temp[0], temp);
         //figure out how to place all argv base into temp, and index the number, and add null at the end
     }
-    else if (child_pid > 0){
-        waitpid(child_pid, NULL, 0);
+    else if (pid > 0){
+        waitpid(pid, NULL, 0);
         printf(i);
     }
     }
