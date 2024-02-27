e04fef6947ab9982424adac35658cd8f77411396
Rubin Jha <wwg6yr@virginia.edu>
2024-02-27 18:19:05 -0500

    R

--- hw4/pt.c
+++ hw4/pt.c
@@ -18,9 +18,7 @@ size_t translate(size_t va){
     if(ptbr == 0){
         return MAX;
     }
-    else if (!(va & 1)){
-        return MAX;
-    }
+
     size_t offset = va & 0xfff;
     size_t index = va >> POBITS;
     size_t PPN = *((size_t *)ptbr + index) >> POBITS;
@@ -40,41 +38,41 @@ void page_allocate(size_t va){
 
 }
 
-// static void set_testing_ptbr(void) {
-//       ptbr = (size_t) &testing_page_table[0];
+static void set_testing_ptbr(void) {
+      ptbr = (size_t) &testing_page_table[0];
 
-//       }
-
-
-//   int main(int argc, char *argv[]) {
-//       alignas(4096)
-//      static size_t page_of_data[512];
-
-//       set_testing_ptbr();
-
-//       alignas(4096)
-//       static char data_for_page_3[4096];
-
-//       size_t address_of_data_for_page_3_as_integer = (size_t) &data_for_page_3[0];
-//       size_t physical_page_number_of_data_for_page_3 = address_of_data_for_page_3_as_integer >> 12;
-//       size_t page_table_entry_for_page_3 = (
-//           (physical_page_number_of_data_for_page_3 << 12)
-//           |
-//           1
-//           );
+      }
 
 
-//       testing_page_table[3] = page_table_entry_for_page_3;
+  int main(int argc, char *argv[]) {
+      alignas(4096)
+     static size_t page_of_data[512];
 
-//       size_t data = (size_t) &data_for_page_3[0x44];
+      set_testing_ptbr();
+
+      alignas(4096)
+      static char data_for_page_3[4096];
+
+      size_t address_of_data_for_page_3_as_integer = (size_t) &data_for_page_3[0];
+      size_t physical_page_number_of_data_for_page_3 = address_of_data_for_page_3_as_integer >> 12;
+      size_t page_table_entry_for_page_3 = (
+          (physical_page_number_of_data_for_page_3 << 12)
+          |
+          1
+          );
 
 
-//       printf("%zx\n", translate(0x3045));
-//       printf("%zx\n", data);
+      testing_page_table[3] = page_table_entry_for_page_3;
 
-//       assert(translate(0x3044) == data);
+      size_t data = (size_t) &data_for_page_3[0x44];
 
 
-//       return 0;
+      printf("%zx\n", translate(0x3045));
+      printf("%zx\n", data);
 
-//       }
+      assert(translate(0x3044) == data);
+
+
+      return 0;
+
+      }
