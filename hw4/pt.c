cd514aaf2753d8f9a50b921d69eba672253f8e66
Rubin Jha <wwg6yr@virginia.edu>
2024-03-19 14:26:56 -0400

    r

--- hw4/pt.c
+++ hw4/pt.c
@@ -6,118 +6,203 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <stdlib.h>
 #define MAX (size_t)(-1)
-#define vpnbits (POBITS - 3)
 
-alignas(4096) static size_t testing_page_table[512];
+alignas(4096)
+  static size_t testing_page_table[512];  
 
 size_t ptbr = 0;
 
-size_t translate(size_t va)
-{
+size_t translate(size_t va){
 
-    if (ptbr == 0)
-    {
+    if(ptbr == 0){
         return MAX;
     }
 
 
-    size_t offset = va & (1 << POBITS) - 1;
-    size_t index = va >> offset;
+    size_t offset = va & 0xfff;
+    size_t index = va >> POBITS;
+    size_t PPN = *((size_t *)ptbr + index) >> POBITS;
+    size_t PPNshifter = PPN << POBITS;
+    size_t PA = PPNshifter | offset;
+    //make sure page table entry is not null
 
-    size_t pos = index;
-
-    size_t count = LEVELS-1;
-
-    size_t curPage = ptbr;
-    for (int i = 0; i < LEVELS; i++)
-    {
-
-        pos = index >> (count * vpnbits);
-        count--;
-        pos = pos & ((1 << vpnbits) - 1);
-        // shift it so it only contains index bits
-        size_t PPN = *((size_t *)curPage + pos) >> POBITS;
-        // shift it back so that offset section is added
-        size_t PPNshifter = PPN << POBITS;
-
-        // make sure page table entry is not null
-
-        // if the pte doesnt exist, then return max
-        size_t PTE = ((size_t *)curPage)[pos];
-        if (!(PTE & 1))
-        {
-            return MAX;
-        }
-
-        if ((PPNshifter != 0))
-        {
-            curPage = PPNshifter;
-        }
-        else
-            return MAX;
+    // if the pte doesnt exist, then return max
+    size_t PTE = ((size_t*)ptbr)[index];
+    if (!(PTE & 1)){
+        return MAX;
     }
 
-        return curPage | offset;
+    if((PA != 0)){
+        return PA;
+    }
+    else
+    return MAX;
+
+
+
 }
 
+void page_allocate(size_t va){
 
-void page_allocate(size_t va)
-{
+}
+
+static void set_testing_ptbr(void) {
+      ptbr = (size_t) &testing_page_table[0];
+
+      }
+
+
+  int main(int argc, char *argv[]) {
+      alignas(4096)
+     static size_t page_of_data[512];
+
+      set_testing_ptbr();
+
+      alignas(4096)
+      static char data_for_page_3[4096];
+
+      size_t address_of_data_for_page_3_as_integer = (size_t) &data_for_page_3[0];
+      size_t physical_page_number_of_data_for_page_3 = address_of_data_for_page_3_as_integer >> 12;
+      size_t page_table_entry_for_page_3 = (
+          (physical_page_number_of_data_for_page_3 << 12)
+          |
+          1
+          );
+
+
+      testing_page_table[3] = page_table_entry_for_page_3;
+
+      size_t data = (size_t) &data_for_page_3[0x44];
+
+
+      printf("%zx\n", translate(0x3045));
+      printf("%zx\n", data);
+
+      assert(translate(0x3044) == data);
+
+
+      return 0;
+
+      }
+// #include "mlpt.h"
+// #include "config.h"
+// #include <assert.h>
+// #include <stdalign.h>
+// #include <string.h>
+// #include <stdio.h>
+// #include <stddef.h>
+// #include <stdint.h>
+// #include <stdlib.h>
+// #define MAX (size_t)(-1)
+// #define vpnbits (POBITS - 3)
+
+// alignas(4096) static size_t testing_page_table[512];
+
+// size_t ptbr = 0;
+
+// size_t translate(size_t va)
+// {
+
+//     if (ptbr == 0)
+//     {
+//         return MAX;
+//     }
+
+
+//     size_t offset = va & (1 << POBITS) - 1;
+//     size_t index = va >> offset;
+
+//     size_t pos = index;
+
+//     size_t count = LEVELS-1;
+
+//     size_t curPage = ptbr;
+//     for (int i = 0; i < LEVELS; i++)
+//     {
+
+//         pos = index >> (count * vpnbits);
+//         count--;
+//         pos = pos & ((1 << vpnbits) - 1);
+//         // shift it so it only contains index bits
+//         size_t PPN = *((size_t *)curPage + pos) >> POBITS;
+//         // shift it back so that offset section is added
+//         size_t PPNshifter = PPN << POBITS;
+
+//         // make sure page table entry is not null
+
+//         // if the pte doesnt exist, then return max
+//         size_t PTE = ((size_t *)curPage)[pos];
+//         if (!(PTE & 1))
+//         {
+//             return MAX;
+//         }
+
+//         if ((PPNshifter != 0))
+//         {
+//             curPage = PPNshifter;
+//         }
+//         else
+//             return MAX;
+//     }
+
+//         return curPage | offset;
+// }
+
+
+// void page_allocate(size_t va)
+// {
 
 
     
 
 
-    size_t offset = va & (1 << POBITS) - 1;
-    size_t index = va >> offset;
+//     size_t offset = va & (1 << POBITS) - 1;
+//     size_t index = va >> offset;
 
-    size_t pos = index;
+//     size_t pos = index;
 
-    size_t count = LEVELS-1;
+//     size_t count = LEVELS-1;
 
-    size_t curPage = ptbr;
-    if (curPage == 0)
-    {
-        curPage = memset(curPage, 0,4096);
-        curPage = posixmemalign(curPage, 4096,4096);
-    }
-    for (int i = 0; i < LEVELS; i++)
-    {
+//     size_t curPage = ptbr;
+//     if (curPage == 0)
+//     {
+//         curPage = memset(curPage, 0,4096);
+//         curPage = posixmemalign(curPage, 4096,4096);
+//     }
+//     for (int i = 0; i < LEVELS; i++)
+//     {
 
-        pos = index >> (count * vpnbits);
-        count--;
-        pos = pos & ((1 << vpnbits) - 1);
-        // shift it so it only contains index bits
-        size_t PPN = *((size_t *)curPage + pos) >> POBITS;
-        // shift it back so that offset section is added
-        size_t PPNshifter = PPN << POBITS;
+//         pos = index >> (count * vpnbits);
+//         count--;
+//         pos = pos & ((1 << vpnbits) - 1);
+//         // shift it so it only contains index bits
+//         size_t PPN = *((size_t *)curPage + pos) >> POBITS;
+//         // shift it back so that offset section is added
+//         size_t PPNshifter = PPN << POBITS;
 
-        // make sure page table entry is not null
+//         // make sure page table entry is not null
 
-        // if the pte doesnt exist, then return max
-        size_t PTE = ((size_t *)curPage)[pos];
-        if (!(PTE & 1))
-        {
-            curPage = memset(curPage, 0,4096);
-            curPage = posixmemalign(curPage, 4096,4096);
-        }
+//         // if the pte doesnt exist, then return max
+//         size_t PTE = ((size_t *)curPage)[pos];
+//         if (!(PTE & 1))
+//         {
+//             curPage = memset(curPage, 0,4096);
+//             curPage = posixmemalign(curPage, 4096,4096);
+//         }
 
-        if ((PPNshifter != 0))
-        {
-            curPage = PPNshifter;
-        }
-        else{
-            curPage = memset(curPage, 0,4096);
-            curPage = posixmemalign(curPage, 4096,4096);
-        }
-    }
-        size_t PA = curPage | offset;
+//         if ((PPNshifter != 0))
+//         {
+//             curPage = PPNshifter;
+//         }
+//         else{
+//             curPage = memset(curPage, 0,4096);
+//             curPage = posixmemalign(curPage, 4096,4096);
+//         }
+//     }
+//         size_t PA = curPage | offset;
 
-}
-
-
+// }
 
 
 
@@ -129,32 +214,34 @@ void page_allocate(size_t va)
 
 
 
-static void set_testing_ptbr(void)
-{
-    ptbr = (size_t)&testing_page_table[0];
-}
 
-int main(int argc, char *argv[])
-{
-    alignas(4096) static size_t page_of_data[512];
 
-    set_testing_ptbr();
+// static void set_testing_ptbr(void)
+// {
+//     ptbr = (size_t)&testing_page_table[0];
+// }
 
-    alignas(4096) static char data_for_page_3[4096];
+// int main(int argc, char *argv[])
+// {
+//     alignas(4096) static size_t page_of_data[512];
 
-    size_t address_of_data_for_page_3_as_integer = (size_t)&data_for_page_3[0];
-    size_t physical_page_number_of_data_for_page_3 = address_of_data_for_page_3_as_integer >> 12;
-    size_t page_table_entry_for_page_3 = ((physical_page_number_of_data_for_page_3 << 12) |
-                                          1);
+//     set_testing_ptbr();
 
-    testing_page_table[3] = page_table_entry_for_page_3;
+//     alignas(4096) static char data_for_page_3[4096];
 
-    size_t data = (size_t)&data_for_page_3[0x44];
+//     size_t address_of_data_for_page_3_as_integer = (size_t)&data_for_page_3[0];
+//     size_t physical_page_number_of_data_for_page_3 = address_of_data_for_page_3_as_integer >> 12;
+//     size_t page_table_entry_for_page_3 = ((physical_page_number_of_data_for_page_3 << 12) |
+//                                           1);
 
-    printf("%zx\n", translate(0x3045));
-    printf("%zx\n", data);
+//     testing_page_table[3] = page_table_entry_for_page_3;
 
-    assert(translate(0x3044) == data);
+//     size_t data = (size_t)&data_for_page_3[0x44];
 
-    return 0;
-}
+//     printf("%zx\n", translate(0x3045));
+//     printf("%zx\n", data);
+
+//     assert(translate(0x3044) == data);
+
+//     return 0;
+// }
