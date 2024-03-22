b37ce4466134afce4dadab28892326b17e39ae8c
Rubin Jha <wwg6yr@virginia.edu>
2024-03-22 13:45:43 -0400

    T

--- hw4/pt.c
+++ hw4/pt.c
@@ -14,43 +14,70 @@
 
 size_t ptbr = 0;
 
-size_t translate(size_t va) {
-    size_t offset = va & ((1 << POBITS) - 1);
+size_t translate(size_t va)
+{
+    
+    // Remove offset bits
     size_t index = va >> POBITS;
-    size_t pos = index;
 
-    size_t count = LEVELS;
-    size_t curPage = ptbr;
-    if (curPage == 0) {
+    // Store value of offset
+    size_t offset = va & ((1 << POBITS) - 1);
+    
+    // Stores index
+    size_t index_Holder = index;
+
+    // Stores levels
+    size_t levelCount = LEVELS;
+
+    // Returns -1 is invalid page
+    if (ptbr == 0)
+    {
         return MAX;
     }
 
-    while (count > 0) {
-        count--;
-        pos = index >> (count * vpnbits);
-        pos = pos & ((1 << vpnbits) - 1);
+    // Store page table register
+    size_t ptbr_Holder = ptbr;
 
-        // Shift it so it only contains index bits
-        size_t ppn = *((size_t *)curPage + pos) >> POBITS;
-        // Shift it back so that offset section is added
+    // Runs through all LEVELS, until it reaches the bottom level
+    while (levelCount > 0)
+    {
+        levelCount--;
+
+        // Remove any higher levels
+        index_Holder = index >> (levelCount * vpnbits);
+
+        // Remove unused bits with a mask
+        index_Holder = index_Holder & ((1 << vpnbits) - 1);
+
+        // Move to index of ptbr and shift through by POBITS to the address
+        size_t ppn = *((size_t *)ptbr_Holder + index_Holder) >> POBITS;
+
+        // Shift left by POBITS to create space
         size_t page_address = ppn << POBITS;
-        size_t pte = ((size_t *)curPage)[pos];
-        if (!(pte & 1)) {
+
+        // Set page table entry equal to the new location
+        size_t pte = ((size_t *)ptbr_Holder)[index_Holder];
+        if (!(pte & 1))
+        {
             return MAX;
         }
-        if ((page_address != 0)) {
-            curPage = page_address;
-        } else {
+        if ((page_address != 0))
+        {
+            ptbr_Holder = page_address;
+        }
+        else
+        {
             return MAX;
         }
     }
-    printf("translate output is %zx: \n", curPage | offset);
-    return curPage | offset;
+    printf("translate output is %zx: \n", ptbr_Holder | offset);
+    return ptbr_Holder | offset;
 }
 
 int countPages = 0;
 
-size_t *insertPageWhenInvalid(size_t ptr) {
+size_t *insertPageWhenInvalid(size_t ptr)
+{
     size_t *pointer;
     int size = pow(2, POBITS);
     posix_memalign((void **)&pointer, size, size);
@@ -59,42 +86,59 @@ size_t *insertPageWhenInvalid(size_t ptr) {
     return pointer;
 }
 
-void page_allocate(size_t va) {
-    // Step 1: Remove offset bits
+void page_allocate(size_t va)
+{
+    // Remove offset bits
     size_t index = va >> POBITS;
 
+    // Stores index
     size_t index_Holder = index;
-    size_t count = LEVELS;
 
-    if (ptbr == 0) {
+    // Stores levels
+    size_t levelCount = LEVELS;
+
+    // Inserts page if there is not already a page
+    if (ptbr == 0)
+    {
         ptbr = (size_t)insertPageWhenInvalid(ptbr);
     }
+
+    // Store page table register
     size_t ptbr_Holder = ptbr;
 
-    while (count > 0) {
-        count--;
-        // Step 2: Remove any higher levels
-        index_Holder = index >> (count * vpnbits);
+    // Runs through all LEVELS, until it reaches the bottom level
+    while (levelCount > 0)
+    {
+        levelCount--;
 
-        // Step 3: Remove unused bits with a mask
+        // Remove any higher levels
+        index_Holder = index >> (levelCount * vpnbits);
+
+        // Remove unused bits with a mask
         index_Holder = index_Holder & ((1 << vpnbits) - 1);
-        // Step 4: Move to index of ptbr and shift through by POBITS to the address
 
+        // Move to index of ptbr and shift through by POBITS to the address
         size_t ppn = *((size_t *)ptbr_Holder + index_Holder) >> POBITS;
-        // Step 5: Shift left by POBITS to create space for it
+
+        // Shift left by POBITS to create space
         size_t page_address = ppn << POBITS;
-        // Step 6: Move to
+
+        // Set page table entry equal to the new location
         size_t pte = ((size_t *)ptbr_Holder)[index_Holder];
-        if (!(pte & 1)) {
+        if (!(pte & 1))
+        {
             pte = (size_t)insertPageWhenInvalid(pte);
-            // Make the valid bit to 1
+
+            // Set new added page address's valid bit to 1
             ((size_t *)ptbr_Holder)[index_Holder] = pte | 1;
+
+            // Move page table base register to the newly allocated page
             ptbr_Holder = pte;
         }
-        if ((page_address != 0)) {
+        // Handles if there is a valid page
+        if ((page_address != 0))
+        {
             ptbr_Holder = page_address;
         }
     }
-
-    // size_t pa = ptbr_Holder | offset;
 }
