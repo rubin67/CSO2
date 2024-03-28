4c7e5fe6f0389fa356431a1333615f2ed2d3b1a4
Rubin Jha <wwg6yr@virginia.edu>
2024-03-28 09:47:40 -0400

    T

--- hw5/prog1.c
+++ hw5/prog1.c
@@ -71,7 +71,7 @@ void prevent_optimizations_based_on_knowing_array_values() {
 int main() {
     const int MAX = 1048568;
     const int SKIP = 4;
-    const int ITERS = 64000000;
+    const int ITERS = 0;
 
 /* these two lines tell Clang (if used to compile this) not to try to 
    perform optimizations on this loop that are likely to make the access
