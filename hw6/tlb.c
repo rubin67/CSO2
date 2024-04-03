b573dbb43d9fb7f8fcaf37a38f4938304793cdf6
Rubin Jha <wwg6yr@virginia.edu>
2024-04-03 19:36:39 -0400

    T

--- hw6/tlb.c
+++ hw6/tlb.c
@@ -132,74 +132,62 @@ size_t tlb_translate(size_t va)
     return pa;
 }
 
-size_t translate(size_t va)
-{
+// size_t translate(size_t va)
+// {
 
-    // Remove offset bits
-    size_t index = va >> POBITS;
+//     // Remove offset bits
+//     size_t index = va >> POBITS;
 
-    // Store value of offset
-    size_t offset = va & ((1 << POBITS) - 1);
+//     // Store value of offset
+//     size_t offset = va & ((1 << POBITS) - 1);
 
-    // Stores index
-    size_t index_Holder = index;
+//     // Stores index
+//     size_t index_Holder = index;
 
-    // Stores levels
-    size_t levelCount = LEVELS;
+//     // Stores levels
+//     size_t levelCount = LEVELS;
 
-    // Returns -1 is invalid page
-    if (ptbr == 0)
-    {
-        return MAX;
-    }
+//     // Returns -1 is invalid page
+//     if (ptbr == 0)
+//     {
+//         return MAX;
+//     }
 
-    // Store page table register
-    size_t ptbr_Holder = ptbr;
+//     // Store page table register
+//     size_t ptbr_Holder = ptbr;
 
-    // Runs through all LEVELS, until it reaches the bottom level
-    while (levelCount > 0)
-    {
-        levelCount--;
+//     // Runs through all LEVELS, until it reaches the bottom level
+//     while (levelCount > 0)
+//     {
+//         levelCount--;
 
-        // Remove any higher levels
-        index_Holder = index >> (levelCount * vpnbits);
+//         // Remove any higher levels
+//         index_Holder = index >> (levelCount * vpnbits);
 
-        // Remove unused bits with a mask
-        index_Holder = index_Holder & ((1 << vpnbits) - 1);
+//         // Remove unused bits with a mask
+//         index_Holder = index_Holder & ((1 << vpnbits) - 1);
 
-        // Move to index of ptbr and shift through by POBITS to the address
-        size_t ppn = *((size_t *)ptbr_Holder + index_Holder) >> POBITS;
+//         // Move to index of ptbr and shift through by POBITS to the address
+//         size_t ppn = *((size_t *)ptbr_Holder + index_Holder) >> POBITS;
 
-        // Shift left by POBITS to create space
-        size_t page_address = ppn << POBITS;
+//         // Shift left by POBITS to create space
+//         size_t page_address = ppn << POBITS;
 
-        // Set page table entry equal to the new location
-        size_t pte = ((size_t *)ptbr_Holder)[index_Holder];
-        if (!(pte & 1))
-        {
-            return MAX;
-        }
-        if ((page_address != 0))
-        {
-            ptbr_Holder = page_address;
-        }
-        else
-        {
-            return MAX;
-        }
-    }
-    printf("translate output is %zx: \n", ptbr_Holder | offset);
-    return ptbr_Holder | offset;
-}
-
-int countPages = 0;
-
-size_t *insertPageWhenInvalid(size_t ptr)
-{
-    size_t *pointer;
-    int size = pow(2, POBITS);
-    posix_memalign((void **)&pointer, size, size);
-    countPages++;
-    memset((void *)pointer, 0, size);
-    return pointer;
-}
+//         // Set page table entry equal to the new location
+//         size_t pte = ((size_t *)ptbr_Holder)[index_Holder];
+//         if (!(pte & 1))
+//         {
+//             return MAX;
+//         }
+//         if ((page_address != 0))
+//         {
+//             ptbr_Holder = page_address;
+//         }
+//         else
+//         {
+//             return MAX;
+//         }
+//     }
+//     printf("translate output is %zx: \n", ptbr_Holder | offset);
+//     return ptbr_Holder | offset;
+// }
