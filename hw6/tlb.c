c1190e69e6121423c3533f76d734a64db14956d7
Rubin Jha <wwg6yr@virginia.edu>
2024-04-04 12:45:15 -0400

    T

--- hw6/tlb.c
+++ hw6/tlb.c
@@ -83,7 +83,7 @@ size_t tlb_translate(size_t va)
 {
 
     size_t vpn = va >> vpnbits;
-    size_t set_index = vpn / NUM_SETS;
+    size_t set_index = vpn % NUM_SETS;
     size_t offset = va & (PAGE_SIZE - 1);
 
     // Check if the translation is already in the TLB
