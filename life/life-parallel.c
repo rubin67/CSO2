714d4f197abe80816015048dea869c1ef4e4bb75
Rubin Jha <wwg6yr@virginia.edu>
2024-04-09 18:24:35 -0400

    T

--- life/life-parallel.c
+++ life/life-parallel.c
@@ -9,8 +9,12 @@ typedef struct
     int start;
     int end;
     int steps;
+    LifeBoard* nextState;
+    LifeBoard* curState;
     pthread_barrier_t *barrier;
 } Threads;
+
+
 
 void simulate_life_parallel(int threads, LifeBoard *state, int steps)
 {
@@ -31,6 +35,10 @@ void simulate_life_parallel(int threads, LifeBoard *state, int steps)
         ThreadArg[i].end = index;
         index++;
         ThreadArg[i].steps = steps;
+        ThreadArg[i].nextState = next_state;
+        ThreadArg[i].curState = state;
+
+
 
         ThreadArg[i].barrier = &barrier;
         pthread_barrier_init(&barrier, NULL, threads);
@@ -58,9 +66,9 @@ void *threadLife(void *info)
         /* We use the range [1, width - 1) here instead of
          * [0, width) because we fix the edges to be all 0s.
          */
-        for (int y = 1; y < state->height - 1; y += 1)
+        for (int y = 1; y < threadInfo[step].curState->height - 1; y += 1)
         {
-            for (int x = 1; x < state->width - 1; x += 1)
+            for (int x = 1; x < threadInfo[step].curState->width - 1; x += 1)
             {
 
                 /* For each cell, examine a 3x3 "window" of cells around it,
@@ -68,19 +76,19 @@ void *threadLife(void *info)
                 int live_in_window = 0;
                 for (int y_offset = -1; y_offset <= 1; y_offset += 1)
                     for (int x_offset = -1; x_offset <= 1; x_offset += 1)
-                        if (LB_get(state, x + x_offset, y + y_offset))
+                        if (LB_get(threadInfo[step].curState, x + x_offset, y + y_offset))
                             live_in_window += 1;
 
                 /* Cells with 3 live neighbors remain or become live.
                    Live cells with 2 live neighbors remain live. */
-                LB_set(threadInfo[i], x, y,
+                LB_set(threadInfo[step].nextState, x, y,
                        live_in_window == 3 /* dead cell with 3 neighbors or live cell with 2 */ ||
-                           (live_in_window == 4 && LB_get(state, x, y)) /* live cell with 3 neighbors */
+                           (live_in_window == 4 && LB_get(threadInfo[step].curState, x, y)) /* live cell with 3 neighbors */
                 );
             }
         }
 
         /* now that we computed next_state, make it the current state */
-        LB_swap(state, next_state);
+        LB_swap(threadInfo[step].curState, threadInfo[step].nextState);
     }
 }
